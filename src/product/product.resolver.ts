4935803f00e258c99bbf99638b612d89741d41c6
Duy Bao <bao1322002@gmail.com>
2024-02-10 20:54:10 +0700

    feat: upload file in firebase

--- src/product/product.resolver.ts
+++ src/product/product.resolver.ts
@@ -3,7 +3,8 @@ import { Product } from './product.definition';
 import { CreateProductInput } from './dto/create-product.input';
 import { OutputType } from 'dryerjs';
 import { FirebaseAdmin, InjectFirebaseAdmin } from 'nestjs-firebase';
-
+import * as fs from 'fs';
+import * as path from 'path';
 @Resolver()
 export class ProductResolver {
   constructor(
@@ -12,8 +13,44 @@ export class ProductResolver {
 
   @Mutation(() => String, { name: 'createProductTest' })
   async create(@Args('input') input: CreateProductInput) {
-    const users = await this.firebase.auth.listUsers(1);
-    console.log(users.users[0].toJSON());
-    return '123';
+    try {
+      // Define the absolute path to the file
+      const filePath = path.resolve(
+        __dirname,
+        '..',
+        '..',
+        'src',
+        'firebase',
+        'test.txt',
+      );
+
+      // Read the file from the local file system
+      const fileBuffer = fs.readFileSync(filePath);
+
+      // Get the specific bucket and create a new blob in the bucket and upload the file data
+      const bucket = this.firebase.storage.bucket('orchid-fer.appspot.com');
+      const fileName = path.basename(filePath);
+      const file = bucket.file(fileName);
+      const writeStream = file.createWriteStream({
+        metadata: {
+          contentType: 'text/plain',
+        },
+      });
+
+      writeStream.on('error', (err) => {
+        console.error('Firebase Storage error:', err);
+      });
+
+      writeStream.on('finish', () => {
+        console.log('File uploaded successfully');
+      });
+
+      writeStream.end(fileBuffer);
+
+      return 'File uploaded successfully';
+    } catch (error) {
+      console.error('Error uploading file:', error);
+      return 'Error uploading file';
+    }
   }
 }
