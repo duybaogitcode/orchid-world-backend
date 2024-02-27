4a112f1f0329b812dfe461d5e50b490befe53e6e
baroxdev <105564773+ThanhPhong31@users.noreply.github.com>
2024-02-27 20:30:28 +0700

    Add additional origin for CORS

--- src/main.ts
+++ src/main.ts
@@ -7,7 +7,11 @@ import * as cookieParser from 'cookie-parser';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: ['http://localhost:3000', 'https://studio.apollographql.com'],
+    origin: [
+      'http://localhost:3000',
+      'https://studio.apollographql.com',
+      'https://orchid-world-frontend.vercel.app',
+    ],
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
     credentials: true,
   });
