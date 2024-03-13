887f5e30269afe9eeba04540733ded9560418944
Duy Bao <bao1322002@gmail.com>
2024-03-13 09:57:57 +0700

    feat: check feedback if input file and check author for task update

--- src/feedbacks/feedback.hook.ts
+++ src/feedbacks/feedback.hook.ts
@@ -5,6 +5,8 @@ import {
   BaseService,
   BeforeFindManyHook,
   BeforeFindManyHookInput,
+  BeforeUpdateHook,
+  BeforeUpdateHookInput,
   InjectBaseService,
   ObjectId,
 } from 'dryerjs';
@@ -12,6 +14,8 @@ import { Context } from 'src/auth/ctx';
 import { UserRole } from 'src/guard/roles.guard';
 import { Feedbacks } from './feedbacks.definition';
 import { Product } from 'src/product/product.definition';
+import { FirebaseService } from 'src/firebase/firebase.serivce';
+import { FileUpload } from 'graphql-upload-ts';
 
 @Injectable()
 export class FeedbackHook {
@@ -20,7 +24,41 @@ export class FeedbackHook {
     public productService: BaseService<Product>,
     @InjectBaseService(Feedbacks)
     public feedbacksService: BaseService<Feedbacks>,
+    private readonly firebase: FirebaseService,
   ) {}
+
+  @BeforeUpdateHook(() => Feedbacks)
+  async beforeUpdateFeedback({
+    input,
+    ctx,
+  }: BeforeUpdateHookInput<Feedbacks, Context>) {
+    const checkFeedback = await this.feedbacksService.model.findById(input.id);
+
+    if (!checkFeedback) {
+      throw new Error('Feedback not exist');
+    }
+
+    if (checkFeedback.authorId.toString() !== ctx.id.toString()) {
+      throw new Error('You are not authorized to edit this feedback');
+    }
+
+    let media = [];
+    if (input.file) {
+      const files = input.file;
+
+      const uploadPromises = files.map(
+        async (filePromise: Promise<FileUpload>) => {
+          const file = await filePromise;
+          return this.firebase.uploadFile(file, 'product');
+        },
+      );
+
+      const urls = await Promise.all(uploadPromises);
+      media = await Promise.all(urls.map((url) => Promise.resolve(url)));
+    }
+
+    input.media = media;
+  }
 
   @AfterUpdateHook(() => Feedbacks)
   async afterUpdateFeedback({ updated }: AfterUpdateHookInput) {
